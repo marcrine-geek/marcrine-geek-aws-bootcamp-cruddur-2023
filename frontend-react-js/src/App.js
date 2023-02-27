76af501efe8bffed04b37fc34fac4b9c1fcc1085
marcrine-geek <marcrinemm@gmail.com>
2023-02-27 16:09:21 +0300

    notifications frontend

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -1,61 +1,62 @@
 import './App.css';
 
-import HomeFeedPage from './pages/HomeFeedPage';
-import UserFeedPage from './pages/UserFeedPage';
-import SignupPage from './pages/SignupPage';
-import SigninPage from './pages/SigninPage';
-import RecoverPage from './pages/RecoverPage';
-import MessageGroupsPage from './pages/MessageGroupsPage';
-import MessageGroupPage from './pages/MessageGroupPage';
-import ConfirmationPage from './pages/ConfirmationPage';
 import React from 'react';
-import process from 'process';
-import {
-  createBrowserRouter,
-  RouterProvider
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ConfirmationPage from './pages/ConfirmationPage';
+import HomeFeedPage from './pages/HomeFeedPage';
+import MessageGroupPage from './pages/MessageGroupPage';
+import MessageGroupsPage from './pages/MessageGroupsPage';
+import NotificationsFeedPage from './pages/NotificationsFeedPage';
+import RecoverPage from './pages/RecoverPage';
+import SigninPage from './pages/SigninPage';
+import SignupPage from './pages/SignupPage';
+import UserFeedPage from './pages/UserFeedPage';
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomeFeedPage />
-  },
-  {
-    path: "/@:handle",
-    element: <UserFeedPage />
-  },
-  {
-    path: "/messages",
-    element: <MessageGroupsPage />
-  },
-  {
-    path: "/messages/@:handle",
-    element: <MessageGroupPage />
-  },
-  {
-    path: "/signup",
-    element: <SignupPage />
-  },
-  {
-    path: "/signin",
-    element: <SigninPage />
-  },
-  {
-    path: "/confirm",
-    element: <ConfirmationPage />
-  },
-  {
-    path: "/forgot",
-    element: <RecoverPage />
-  }
+	{
+		path: '/',
+		element: <HomeFeedPage />,
+	},
+	{
+		path: '/notifications',
+		element: <NotificationsFeedPage />,
+	},
+	{
+		path: '/@:handle',
+		element: <UserFeedPage />,
+	},
+	{
+		path: '/messages',
+		element: <MessageGroupsPage />,
+	},
+	{
+		path: '/messages/@:handle',
+		element: <MessageGroupPage />,
+	},
+	{
+		path: '/signup',
+		element: <SignupPage />,
+	},
+	{
+		path: '/signin',
+		element: <SigninPage />,
+	},
+	{
+		path: '/confirm',
+		element: <ConfirmationPage />,
+	},
+	{
+		path: '/forgot',
+		element: <RecoverPage />,
+	},
 ]);
 
 function App() {
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
+	return (
+		<>
+			<RouterProvider router={router} />
+		</>
+	);
 }
 
 export default App;
