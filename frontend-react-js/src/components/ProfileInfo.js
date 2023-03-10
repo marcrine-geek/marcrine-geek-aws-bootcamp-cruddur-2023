90973389af9af25c20d69d7227a0d63faf72ba6f
marcrine-geek <marcrinemm@gmail.com>
2023-03-10 16:22:21 +0300

    updates

--- frontend-react-js/src/components/ProfileInfo.js
+++ frontend-react-js/src/components/ProfileInfo.js
@@ -1,50 +1,51 @@
+import React from 'react';
 import './ProfileInfo.css';
-import {ReactComponent as ElipsesIcon} from './svg/elipses.svg';
-import React from "react";
+import { ReactComponent as ElipsesIcon } from './svg/elipses.svg';
 
 // [TODO] Authenication
-import Cookies from 'js-cookie'
+import { Auth } from 'aws-amplify';
 
 export default function ProfileInfo(props) {
-  const [popped, setPopped] = React.useState(false);
+	const [popped, setPopped] = React.useState(false);
 
-  const click_pop = (event) => {
-    setPopped(!popped)
-  }
+	const click_pop = (event) => {
+		setPopped(!popped);
+	};
 
-  const signOut = async () => {
-    console.log('signOut')
-    // [TODO] Authenication
-    Cookies.remove('user.logged_in')
-    //Cookies.remove('user.name')
-    //Cookies.remove('user.username')
-    //Cookies.remove('user.email')
-    //Cookies.remove('user.password')
-    //Cookies.remove('user.confirmation_code')
-    window.location.href = "/"
-  }
+	const signOut = async () => {
+		try {
+			await Auth.signOut({ global: true });
+			window.location.href = '/';
+		} catch (error) {
+			console.log('error signing out: ', error);
+		}
+	};
 
-  const classes = () => {
-    let classes = ["profile-info-wrapper"];
-    if (popped == true){
-      classes.push('popped');
-    }
-    return classes.join(' ');
-  }
+	const classes = () => {
+		let classes = ['profile-info-wrapper'];
+		if (popped == true) {
+			classes.push('popped');
+		}
+		return classes.join(' ');
+	};
 
-  return (
-    <div className={classes()}>
-      <div className="profile-dialog">
-        <button onClick={signOut}>Sign Out</button> 
-      </div>
-      <div className="profile-info" onClick={click_pop}>
-        <div className="profile-avatar"></div>
-        <div className="profile-desc">
-          <div className="profile-display-name">{props.user.display_name || "My Name" }</div>
-          <div className="profile-username">@{props.user.handle || "handle"}</div>
-        </div>
-        <ElipsesIcon className='icon' />
-      </div>
-    </div>
-  )
+	return (
+		<div className={classes()}>
+			<div className='profile-dialog'>
+				<button onClick={signOut}>Sign Out</button>
+			</div>
+			<div className='profile-info' onClick={click_pop}>
+				<div className='profile-avatar'></div>
+				<div className='profile-desc'>
+					<div className='profile-display-name'>
+						{props.user.display_name || 'My Name'}
+					</div>
+					<div className='profile-username'>
+						@{props.user.handle || 'handle'}
+					</div>
+				</div>
+				<ElipsesIcon className='icon' />
+			</div>
+		</div>
+	);
 }
--- frontend-react-js/src/pages/SigninPage.js
+++ frontend-react-js/src/pages/SigninPage.js
@@ -1,86 +1,87 @@
+import { Auth } from 'aws-amplify';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../components/svg/logo.svg';
 import './SigninPage.css';
-import React from "react";
-import {ReactComponent as Logo} from '../components/svg/logo.svg';
-import { Link } from "react-router-dom";
 
 // [TODO] Authenication
-import Cookies from 'js-cookie'
 
 export default function SigninPage() {
+	const [email, setEmail] = React.useState('');
+	const [password, setPassword] = React.useState('');
+	const [errors, setErrors] = React.useState('');
 
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const [errors, setErrors] = React.useState('');
+	const onsubmit = async (event) => {
+		setErrors('');
+		event.preventDefault();
+		try {
+			Auth.signIn(email, password)
+				.then((user) => {
+					localStorage.setItem(
+						'access_token',
+						user.signInUserSession.accessToken.jwtToken
+					);
+					window.location.href = '/';
+				})
+				.catch((err) => {
+					console.log('Error!', err);
+				});
+		} catch (error) {
+			if (error.code == 'UserNotConfirmedException') {
+				window.location.href = '/confirm';
+			}
+			setErrors(error.message);
+		}
+		return false;
+	};
 
-  const onsubmit = async (event) => {
-    event.preventDefault();
-    setErrors('')
-    console.log('onsubmit')
-    if (Cookies.get('user.email') === email && Cookies.get('user.password') === password){
-      Cookies.set('user.logged_in', true)
-      window.location.href = "/"
-    } else {
-      setErrors("Email and password is incorrect or account doesn't exist")
-    }
-    return false
-  }
+	const email_onchange = (event) => {
+		setEmail(event.target.value);
+	};
+	const password_onchange = (event) => {
+		setPassword(event.target.value);
+	};
 
-  const email_onchange = (event) => {
-    setEmail(event.target.value);
-  }
-  const password_onchange = (event) => {
-    setPassword(event.target.value);
-  }
+	let el_errors;
+	if (errors) {
+		el_errors = <div className='errors'>{errors}</div>;
+	}
 
-  let el_errors;
-  if (errors){
-    el_errors = <div className='errors'>{errors}</div>;
-  }
-
-  return (
-    <article className="signin-article">
-      <div className='signin-info'>
-        <Logo className='logo' />
-      </div>
-      <div className='signin-wrapper'>
-        <form 
-          className='signin_form'
-          onSubmit={onsubmit}
-        >
-          <h2>Sign into your Cruddur account</h2>
-          <div className='fields'>
-            <div className='field text_field username'>
-              <label>Email</label>
-              <input
-                type="text"
-                value={email}
-                onChange={email_onchange} 
-              />
-            </div>
-            <div className='field text_field password'>
-              <label>Password</label>
-              <input
-                type="password"
-                value={password}
-                onChange={password_onchange} 
-              />
-            </div>
-          </div>
-          {el_errors}
-          <div className='submit'>
-            <Link to="/forgot" className="forgot-link">Forgot Password?</Link>
-            <button type='submit'>Sign In</button>
-          </div>
-
-        </form>
-        <div className="dont-have-an-account">
-          <span>
-            Don't have an account?
-          </span>
-          <Link to="/signup">Sign up!</Link>
-        </div>
-      </div>
-
-    </article>
-  );
+	return (
+		<article className='signin-article'>
+			<div className='signin-info'>
+				<Logo className='logo' />
+			</div>
+			<div className='signin-wrapper'>
+				<form className='signin_form' onSubmit={onsubmit}>
+					<h2>Sign into your Cruddur account</h2>
+					<div className='fields'>
+						<div className='field text_field username'>
+							<label>Email</label>
+							<input type='text' value={email} onChange={email_onchange} />
+						</div>
+						<div className='field text_field password'>
+							<label>Password</label>
+							<input
+								type='password'
+								value={password}
+								onChange={password_onchange}
+							/>
+						</div>
+					</div>
+					{el_errors}
+					<div className='submit'>
+						<Link to='/forgot' className='forgot-link'>
+							Forgot Password?
+						</Link>
+						<button type='submit'>Sign In</button>
+					</div>
+				</form>
+				<div className='dont-have-an-account'>
+					<span>Don't have an account?</span>
+					<Link to='/signup'>Sign up!</Link>
+				</div>
+			</div>
+		</article>
+	);
 }
